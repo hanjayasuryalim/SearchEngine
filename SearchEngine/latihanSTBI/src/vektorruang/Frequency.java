696c91ca9a25795f3e2b5f3613ea135af7ff1361
Alexander Tri Handoyo Wangsaputra <ATHWangsaputra@gmail.com>
2020-04-19 22:08:15 +0700

    Add function to count frequency in query

--- SearchEngine/latihanSTBI/src/vektorruang/Frequency.java
+++ SearchEngine/latihanSTBI/src/vektorruang/Frequency.java
@@ -1,5 +1,6 @@
 package vektorruang;
 
+import Model.NoDocValue;
 import Utility.WriteFile;
 import java.io.File;
 import java.util.ArrayList;
@@ -15,7 +16,16 @@ import java.util.List;
 
 public class Frequency {
 
+    private ArrayList<String> termDate;
+    private ArrayList<String> termTitle;
+    private ArrayList<String> termBody;
+
+    private HashMap<String, ArrayList<NoDocFrequency>> dictDate;
+    private HashMap<String, ArrayList<NoDocFrequency>> dictTitle;
+    private HashMap<String, ArrayList<NoDocFrequency>> dictBody;
+
     class NoDocFrequency {
+
         int noDoc;
         int frequency;
 
@@ -27,12 +37,39 @@ public class Frequency {
 
     public void countTermFrequency() {
         //To store key
-        ArrayList<String> termDate = new ArrayList<>();
-        ArrayList<String> termTitle = new ArrayList<>();
-        ArrayList<String> termBody = new ArrayList<>();
+        termDate = new ArrayList<>();
+        termTitle = new ArrayList<>();
+        termBody = new ArrayList<>();
 
+        //To get all terms
+        getAllTermsInDocuments();
+
+        //To store frequency
+        dictDate = new HashMap<>();
+        dictTitle = new HashMap<>();
+        dictBody = new HashMap<>();
+
+        //To initialize all dictionary
+        initializeDictionary();
+
+        //For frequency
+        getFrequencyFromDocuments();
+
+        //Saving the frequency
+        System.out.println("Frequency of Date : ");
+        csvDict(dictDate, "Date", "XML/FrequencyDate.csv");
+
+        System.out.println("Frequency of Title : ");
+        csvDict(dictTitle, "Title", "XML/FrequencyTitle.csv");
+
+        System.out.println("Frequency of Body : ");
+        csvDict(dictBody, "Body", "XML/FrequencyBody.csv");
+    }
+
+    private void getAllTermsInDocuments() {
         int n = 500;
 
+        //To get all term in documents
         for (int i = 1; i <= n; i++) {
             String from = "XML/Doc (" + i + ").xml";
 
@@ -90,12 +127,9 @@ public class Frequency {
                 e.printStackTrace();
             }
         }
+    }
 
-        //To store frequency
-        HashMap<String, ArrayList<NoDocFrequency>> dictDate = new HashMap<>();
-        HashMap<String, ArrayList<NoDocFrequency>> dictTitle = new HashMap<>();
-        HashMap<String, ArrayList<NoDocFrequency>> dictBody = new HashMap<>();
-        
+    private void initializeDictionary() {
         //Initialize dictDate
         Collections.sort(termDate);
         for (int i = 0; i < termDate.size(); i++) {
@@ -119,8 +153,12 @@ public class Frequency {
             ArrayList<NoDocFrequency> value = new ArrayList<>();
             dictBody.put(key, value);
         }
+    }
 
-        //For frequency
+    private void getFrequencyFromDocuments() {
+        int n = 500;
+
+        //To count frequency
         for (int i = 1; i <= n; i++) {
             String from = "XML/Doc (" + i + ").xml";
 
@@ -137,10 +175,10 @@ public class Frequency {
 
                 String date = (doc.getElementsByTagName("date").getLength() <= 0) ? "" : doc.getElementsByTagName("date").item(0).getTextContent();
                 System.out.println(date);
-                while(date.contains("  ")){
+                while (date.contains("  ")) {
                     date = date.replace("  ", " ");
                 }
-                
+
                 List<String> arrDate = Arrays.asList(date.split(" "));
                 System.out.println(date);
                 for (String key : termDate) {
@@ -149,16 +187,16 @@ public class Frequency {
                         int frequency = Collections.frequency(arrDate, key);
 
                         NoDocFrequency newValue = new NoDocFrequency(noDoc, frequency);
-                        dictDate.get(key).add(i-1, newValue);
+                        dictDate.get(key).add(i - 1, newValue);
                     } else {
                         NoDocFrequency newValue = new NoDocFrequency(i, 0);
-                        dictDate.get(key).add(i-1, newValue);
+                        dictDate.get(key).add(i - 1, newValue);
                     }
                 }
 
                 String title = (doc.getElementsByTagName("title").getLength() <= 0) ? "" : doc.getElementsByTagName("title").item(0).getTextContent();
 
-                while(title.contains("  ")){
+                while (title.contains("  ")) {
                     title = title.replace("  ", " ");
                 }
                 List<String> arrTitle = Arrays.asList(title.split(" "));
@@ -168,16 +206,16 @@ public class Frequency {
                         int frequency = Collections.frequency(arrTitle, key);
 
                         NoDocFrequency newValue = new NoDocFrequency(noDoc, frequency);
-                        dictTitle.get(key).add(i-1, newValue);
+                        dictTitle.get(key).add(i - 1, newValue);
                     } else {
                         NoDocFrequency newValue = new NoDocFrequency(i, 0);
-                        dictTitle.get(key).add(i-1, newValue);
+                        dictTitle.get(key).add(i - 1, newValue);
                     }
                 }
 
                 String body = (doc.getElementsByTagName("body").getLength() <= 0) ? "" : doc.getElementsByTagName("body").item(0).getTextContent();
 
-                while(body.contains("  ")){
+                while (body.contains("  ")) {
                     body = body.replace("  ", " ");
                 }
                 List<String> arrBody = Arrays.asList(body.split(" "));
@@ -187,54 +225,44 @@ public class Frequency {
                         int frequency = Collections.frequency(arrBody, key);
 
                         NoDocFrequency newValue = new NoDocFrequency(noDoc, frequency);
-                        dictBody.get(key).add(i-1, newValue);
+                        dictBody.get(key).add(i - 1, newValue);
                     } else {
                         NoDocFrequency newValue = new NoDocFrequency(i, 0);
-                        dictBody.get(key).add(i-1, newValue);
+                        dictBody.get(key).add(i - 1, newValue);
                     }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-
-        //Saving the frequency
-        System.out.println("Frequency of Date : ");
-        csvDict(dictDate, "Date", "XML/FrequencyDate.csv");
-
-        System.out.println("Frequency of Title : ");
-        csvDict(dictTitle, "Title", "XML/FrequencyTitle.csv");
-
-        System.out.println("Frequency of Body : ");
-        csvDict(dictBody, "Body", "XML/FrequencyBody.csv");
     }
 
     private static String csvDict(HashMap<String, ArrayList<NoDocFrequency>> dict, String category, String target) {
         String data = "";
         Set<String> keySort = dict.keySet();
 
-        List<String> keys =  StringSorter.sortStringList(new ArrayList<String>(keySort));
+        List<String> keys = StringSorter.sortStringList(new ArrayList<String>(keySort));
 
         data = "";
 
-        for (int j = 0; j < keys.size();j++) {
+        for (int j = 0; j < keys.size(); j++) {
             String key = keys.get(j);
-            if (!key.equals("")){
-              data += key + "= [";
+            if (!key.equals("")) {
+                data += key + "= [";
 
-              ArrayList<NoDocFrequency> values = dict.get(key);
-              for (int i = 0; i < values.size(); i++) {
-                  data += String.valueOf(values.get(i).frequency) + ", ";
-              }
+                ArrayList<NoDocFrequency> values = dict.get(key);
+                for (int i = 0; i < values.size(); i++) {
+                    data += String.valueOf(values.get(i).frequency) + ", ";
+                }
 
-              data = data.substring(0, data.length()-2);
-              data +="]\n";
-               
-              System.out.println(data);
+                data = data.substring(0, data.length() - 2);
+                data += "]\n";
 
-              WriteFile.writeData(data, target);
-              data = "";
-              System.out.println(" ");
+                System.out.println(data);
+
+                WriteFile.writeData(data, target);
+                data = "";
+                System.out.println(" ");
             }
         }
         data += "\n\n";
@@ -248,5 +276,43 @@ public class Frequency {
     public static void main(String[] args) {
         Frequency f = new Frequency();
         f.countTermFrequency();
+    }
+
+    private static ArrayList<String> getAllTermsInQuery(String query) {
+        ArrayList<String> queryTerms = new ArrayList<>();
+
+        String[] arrQuery = query.split(" ");
+        for (String word : arrQuery) {
+            if (!queryTerms.contains(word)) {
+                queryTerms.add(word);
+            }
+        }
+
+        return queryTerms;
+    }
+
+    private static int getTermsFrequency(String term, String sentence) {
+        //Query to list
+        List<String> arrSentences = Arrays.asList(sentence.split(" "));
+
+        int frequency = Collections.frequency(arrSentences, term);
+
+        return frequency;
+    }
+
+    public static HashMap<String, NoDocValue> getQueryFrequency(String query) {
+        HashMap<String, NoDocValue> queryFrequency = new HashMap<>();
+
+        //Get all terms in the query
+        ArrayList<String> queryTerms = getAllTermsInQuery(query);
+
+        //Initialize HashMap
+        for (String term : queryTerms) {
+            int frequency = getTermsFrequency(term, query);
+            NoDocValue value = new NoDocValue(0, Double.valueOf(frequency));
+            queryFrequency.put(term, value);
+        }
+
+        return queryFrequency;
     }
 }
