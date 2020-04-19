4f0b9f02a5e605461d9cca8a803b9e1caa65b55a
Alexander Tri Handoyo Wangsaputra <ATHWangsaputra@gmail.com>
2020-04-19 22:08:43 +0700

    Add function to read weight CSV

--- SearchEngine/latihanSTBI/src/Utility/ReadCSV.java
+++ SearchEngine/latihanSTBI/src/Utility/ReadCSV.java
@@ -13,69 +13,114 @@ import java.lang.String;
 import Model.NoDocValue;
 
 public class ReadCSV {
-  
-   
-    public static HashMap<String,ArrayList<NoDocValue>> getDataCSV(String path) {
-        HashMap<String,ArrayList<NoDocValue>> returnMap = new HashMap<>(); 
-       
+
+    public static HashMap<String, ArrayList<NoDocValue>> getDataCSVWeight(String path) {
+        HashMap<String, ArrayList<NoDocValue>> returnMap = new HashMap<>();
+        
         String row;
+        int noDoc;
         double value;
         
-        try{
+        System.out.println("Reading " + path + " : ");
+        System.out.println("--------------------------------------");
+        System.out.println("Start");
+        
+        try {
             BufferedReader csvReader = new BufferedReader(new FileReader(path));
-            while((row = csvReader.readLine())!=null){
+            while ((row = csvReader.readLine()) != null) {
                 ArrayList<NoDocValue> docList = new ArrayList<>();
-                String [] data = null;
-                String [] subData = null;
-                
+                String[] data = null;
+                String[] subData = null;
+
                 data = row.split("=");
-                data[1] = data[1].replace("[","").replace("]", "").replace(" ", "").replace(";","");
+                data[1] = data[1].replace("[", "").replace("]", "").replace(" ", "").replace(";", "");
                 subData = data[1].split(",");
                 
+                System.out.print(data[0] + ": ");
+                //Get document no document and value
+                for (String sub : subData) {
+                    String[] noDocAndValue = sub.replace(" ", "").split("\\|");
+                    System.out.print("[" + noDocAndValue[0] + "|" + noDocAndValue[1] + "]");
+                    
+                    value = Double.parseDouble(noDocAndValue[1]);
+                    noDoc = Integer.parseInt(noDocAndValue[0]);
+                    NoDocValue doc = new NoDocValue(noDoc, value);
+                    docList.add(doc);
+                }
+                System.out.println("");
+                
+                returnMap.put(data[0], docList);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("End");
+        System.out.println("--------------------------------------");
+        return returnMap;
+    }
+
+    public static HashMap<String, ArrayList<NoDocValue>> getDataCSV(String path) {
+        HashMap<String, ArrayList<NoDocValue>> returnMap = new HashMap<>();
+
+        String row;
+        double value;
+
+        try {
+            BufferedReader csvReader = new BufferedReader(new FileReader(path));
+            while ((row = csvReader.readLine()) != null) {
+                ArrayList<NoDocValue> docList = new ArrayList<>();
+                String[] data = null;
+                String[] subData = null;
+
+                data = row.split("=");
+                data[1] = data[1].replace("[", "").replace("]", "").replace(" ", "").replace(";", "");
+                subData = data[1].split(",");
+
                 for (int i = 0; i < 500; i++) {
                     value = Double.parseDouble(subData[i]);
-                    NoDocValue doc = new NoDocValue(i,value);
+                    NoDocValue doc = new NoDocValue(i, value);
                     docList.add(doc);
                 }
                 returnMap.put(data[0], docList);
-               
+
             }
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return returnMap;
     }
-    
-    public static HashMap<String,Double> getDataCSVIDF(String path) {
-        HashMap<String,Double> returnMap = new HashMap<>(); 
-       
+
+    public static HashMap<String, Double> getDataCSVIDF(String path) {
+        HashMap<String, Double> returnMap = new HashMap<>();
+
         String row;
         double value;
-        
-        try{
+
+        try {
             BufferedReader csvReader = new BufferedReader(new FileReader(path));
-            while((row = csvReader.readLine())!=null){
-               
-                String [] data = null;
-                String [] subData = null;
-                
+            while ((row = csvReader.readLine()) != null) {
+
+                String[] data = null;
+                String[] subData = null;
+
                 data = row.split("=");
-               
-              
-               
-                returnMap.put(data[0],Double.parseDouble(data[1]));
-               
+
+                returnMap.put(data[0], Double.parseDouble(data[1]));
+
             }
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return returnMap;
     }
-    
+
 //    public static void main(String[] args) {
 //       HashMap<String,ArrayList<NoDocValue>> returnMap = new HashMap<>(); 
 //       
@@ -106,7 +151,6 @@ public class ReadCSV {
 //        }     
 //        
 //    }
-    
 //    public static void main(String[] args) {
 //        HashMap<String,ArrayList<NoDocValue>> returnMap = new HashMap<>(); 
 //       
